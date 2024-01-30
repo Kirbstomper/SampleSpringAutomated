5bd6f5167ed59d926b118a8a0104bc652eeb511e
Chris Smith <wingedkraby@gmail.com>
2024-01-30 10:40:19 -0500

    First commit

---
+++ src/main/java/com/kirbstomper/AutomatedSpringDeployment/AutomatedSpringDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.kirbstomper.AutomatedSpringDeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedSpringDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedSpringDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirbstomper/AutomatedSpringDeployment/AutomatedSpringDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirbstomper.AutomatedSpringDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedSpringDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
